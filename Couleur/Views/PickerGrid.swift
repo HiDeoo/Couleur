2dcf8d51811c18ffb796c6a335390298d6e63f65
HiDeo <hideo.sup@gmail.com>
2020-01-16 16:06:26 +0100

    Improve picker grid UI

--- Couleur/Views/PickerGrid.swift
+++ Couleur/Views/PickerGrid.swift
@@ -12,20 +12,31 @@ struct PickerGrid: View {
   let lineCount = Int(Constants.PickerPointCount + 2)
 
   var body: some View {
-    Path { path in
-      for index in 0 ... lineCount {
-        let offset = Constants.PickerPointSize * CGFloat(index)
-        path.move(to: CGPoint(x: offset, y: 0))
-        path.addLine(to: CGPoint(x: offset, y: Constants.PickerSize))
-      }
+    ZStack {
+      Path { path in
+        for index in 0 ... lineCount {
+          let offset = Constants.PickerPointSize * CGFloat(index) - 1
+          path.move(to: CGPoint(x: offset, y: 0))
+          path.addLine(to: CGPoint(x: offset, y: Constants.PickerSize))
+        }
 
-      for index in 0 ... lineCount {
-        let offset = Constants.PickerPointSize * CGFloat(index)
-        path.move(to: CGPoint(x: 0, y: offset))
-        path.addLine(to: CGPoint(x: Constants.PickerSize, y: offset))
+        for index in 0 ... lineCount {
+          let offset = Constants.PickerPointSize * CGFloat(index) - 1
+          path.move(to: CGPoint(x: 0, y: offset))
+          path.addLine(to: CGPoint(x: Constants.PickerSize, y: offset))
+        }
       }
+      .stroke(Color.black.opacity(0.5))
+      Path { path in
+        let origin = floor(Constants.PickerPointCount / 2) * Constants.PickerPointSize - 2
+        path.move(to: CGPoint(x: origin, y: origin))
+        path.addLine(to: CGPoint(x: origin + Constants.PickerPointSize + 2, y: origin))
+        path.addLine(to: CGPoint(x: origin + Constants.PickerPointSize + 2, y: origin + Constants.PickerPointSize + 2))
+        path.addLine(to: CGPoint(x: origin, y: origin + Constants.PickerPointSize + 2))
+        path.addLine(to: CGPoint(x: origin, y: origin - 0.5))
+      }
+      .stroke(Color.white)
     }
-    .stroke()
     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
   }
 }
